407748831f5bb4ceedd2c613abf00793856b98a0
iiPython <ben@iipython.dev>
2024-05-25 13:51:08 -0500

    last update for today (and hopefully ever)

--- nova/assets/spa.js
+++ nova/assets/spa.js
@@ -6,18 +6,9 @@ const replace = document.querySelector("%s");
 for (const link of document.getElementsByTagName("a")) {
     const relative = link.href.slice(length);
     if (!pages.includes(relative)) continue;
-    const load_page = async () => {
-        cache[relative] = await (await fetch(`/pages${relative === '/' ? '/index' : relative}`)).text();
-    }
-    const handle_hover = () => {
-        if (cache[relative]) return;
-        load_page()
-        removeEventListener("mouseover", handle_hover);
-    }
-    link.addEventListener("mouseover", handle_hover);
-    link.addEventListener("click", (e) => {
+    link.addEventListener("click", async (e) => {
         e.preventDefault();
-        if (!cache[relative]) load_page();
+        if (!cache[relative]) cache[relative] = await (await fetch(`/pages${relative === '/' ? '/index' : relative}`)).text();
         const title = relative.slice(1);
         document.title = `%s${title && ('%s' + title[0].toUpperCase() + title.slice(1))}`;
         replace.innerHTML = "";
