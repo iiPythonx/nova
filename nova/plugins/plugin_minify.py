c49342ccc08aa6e0a316bd44f0e0b49bef63eab4
iiPython <ben@iipython.dev>
2024-06-20 12:50:37 -0500

    0.7.4: add better css minification (removing minify-html soon)

--- nova/plugins/plugin_minify.py
+++ nova/plugins/plugin_minify.py
@@ -42,11 +42,16 @@ class MinifyPlugin():
             rcon.print("[yellow]\u26a0  The minify plugin requires uglifyjs in order to perform JS minification.[/]")
             self.config["suffixes"].remove(".js")
 
+        # Check for csso-cli
+        if ".css" in self.config["suffixes"] and not shutil.which("csso"):
+            rcon.print("[yellow]\u26a0  The minify plugin requires csso-cli in order to perform CSS minification.[/]")
+            self.config["suffixes"].remove(".css")
+
     def _minify_js(self, path: Path) -> None:
         subprocess.run(["uglifyjs", path, "-c", "-m", "-o", path])
 
     def _minify_css(self, path: Path) -> None:
-        path.write_text(minify_html.minify("<style>" + path.read_text("utf8"))[8:], "utf8")
+        subprocess.run(["csso", "-i", path, "-o", path])
 
     def _minify_html(self, path: Path) -> None:
         path.write_text(minify_html.minify(path.read_text("utf8"), **self.options), "utf8")
